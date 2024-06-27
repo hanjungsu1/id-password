b72aeddcba3c79a49397375790928c9c03deab76
한정수 <wjdtn1642@naver.com>
2024-06-28 00:26:22 +0900

    기본 개인정보 입력 기능 추가

--- newusers.js
+++ newusers.js
@@ -16,3 +16,65 @@ function removeBlank(theElement) {
     }
     theElement.value = tempvalue;
 }
+
+function CheckInputData() {
+    if (document.newuserform.id.value.length == 0) {
+        alert('ID를 입력하세요');
+        document.newuserform.id.focus();
+        return false;
+    }
+    if (document.newuserform.pwd.value.length == 0) {
+        alert('암호를 입력하세요');
+        document.newuserform.pwd.focus();
+        return false;
+    }
+    if (document.newuserform.pwdcf.value.length == 0) {
+        alert('암호 확인을 입력하세요');
+        document.newuserform.pwdcf.focus();
+        return false;
+    }
+    if (document.newuserform.pwd.value != document.newuserform.pwdcf.value) {
+        alert('암호 확인을 다시 입력하세요');
+        document.newuserform.pwdcf.focus();
+        return false;
+    }
+    if (document.newuserform.sno1.value.length > 0 || document.newuserform.sno2.value.length > 0) {
+        if (document.newuserform.sno1.value.length != 6 || isNaN(document.newuserform.sno1.value)) {
+            alert('주민등록번호 앞자리를 다시 입력하세요');
+            document.newuserform.sno1.focus();
+            return false; 
+        }
+        if (document.newuserform.sno2.value.length != 7 || isNaN(document.newuserform.sno2.value)) {
+            alert('주민등록번호 뒷자리를 다시 입력하세요');
+            document.newuserform.sno2.focus();
+            return false; 
+        }
+    }
+    if (document.newuserform.tel1.value.length > 0 || document.newuserform.tel2.value.length > 0 \\ document.newuserform.tel3.value.length > 0) {
+        if (document.newuserform.tel1.value.length < 2 || isNaN(document.newuserform.tel2.value)) {
+            alert('지역번호를 다시 입력하세요');
+            document.newuserform.tel1.focus();
+            return false;    
+        }
+        if (document.newuserform.tel2.value.length == 0 || isNaN(document.newuserform.tel2.value)) {
+            alert('국번을 다시 입력하세요');
+            document.newuserform.tel2.focus();
+            return false;    
+        }
+        if (document.newuserform.tel3.value.length == 0 || isNaN(document.newuserform.tel3.value)) {
+            alert('전화번호를 다시 입력하세요');
+            document.newuserform.tel3.focus();
+            return false;    
+        }       
+    }
+    if (document.newuserform.job.options[document.newuserform.job.selectedIndex].value == '0') {
+        alert('직업을 선택하세요');
+        document.newuserform.job.focus();
+        return false;
+    }
+    if (document.newuserform.married[0].checked == false && document.newuserform.marrried[1].checked == false) {
+        alert('결혼 여부를 선택하세요');
+        return false;
+    }
+    return true;
+}
